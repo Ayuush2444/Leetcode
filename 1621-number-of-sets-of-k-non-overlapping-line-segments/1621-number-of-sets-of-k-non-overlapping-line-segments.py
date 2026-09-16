01de1e2d8a05c7badaf65b06ae96fb9787ab2628
Ayush <100771119+Ayuush2444@users.noreply.github.com>
2026-09-16 11:05:52 +0530

    Time: 1125 ms (29.31%), Space: 66.3 MB (34.48%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -5,7 +5,6 @@ class Solution:
         dp = [[0] * (k + 1) for _ in range(n)]
         prefix = [[0] * (k + 1) for _ in range(n)]
 
-        # Zero segments = 1 way for every prefix
         for i in range(n):
             dp[i][0] = 1
             prefix[i][0] = i + 1
@@ -13,16 +12,13 @@ class Solution:
         for i in range(1, n):
             for j in range(1, k + 1):
 
-                # Don't use point i
                 dp[i][j] = dp[i - 1][j]
 
-                # Add a segment ending at i
                 if i >= 1:
                     dp[i][j] += prefix[i - 1][j - 1]
 
                 dp[i][j] %= MOD
 
-                # Update prefix sum
                 prefix[i][j] = prefix[i - 1][j] + dp[i][j]
                 prefix[i][j] %= MOD
 
